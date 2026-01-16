1ae1d86e2a655bf301892edc8913ff79c6db2ce4
Bryce? <bryce.jones2026@gbwl.org>
2026-01-16 14:41:00 +0000

    PAIN AND SUFFERING

--- main.py
+++ main.py
@@ -12,12 +12,17 @@ Terraria = pd.read_csv('Terraria DPS_TV1.4.4.9_V3.csv')
 dps = Terraria['DPS (SINGLE TARGET)'] #y value
 game_progress = Terraria['GAME PROGRESSION']
 multdps = Terraria['DPS (MULTI TARGET)']
+#Drop nan
 name = Terraria['NAME']
 plt.bar(game_progress, dps)
 plt.xticks(rotation=45)
 plt.savefig('dpsvgameprogress.png', bbox_inches='tight')
 plt.close()
 
-
-plt.pie(name, multdps.bool(), startangle=45)
-plt.savefig('multpie.png')
+Terraria.set_index('DPS (MULTI TARGET)')
+a = Terraria.ix[[p.id, 'nan']]
+print(a)
+# Create subset that counts all weapons with multi target dps
+# Create subset that counts weapons without multi target dps
+#plt.pie(multdps, multdps.values, startangle=45)
+#plt.savefig('multpie.png')
